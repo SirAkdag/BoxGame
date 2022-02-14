b430118797c6390bb7fc0b9db7fa5f964ce31bbe
Emre Akdag <emreakdag@windowslive.com>
2022-02-14 21:25:04 +0100

    yeni proje

---
+++ src/Match.java
@@ -0,0 +1,31 @@
+public class Match {
+
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public void run() {
+        if (isCheck()) {
+            while (this.f1.health>0 && this.f2.health>0){
+
+            }
+        } else {
+            System.out.println("Sporcularin sikletleri uyusmuyor !");
+        }
+    }
+
+
+    boolean isCheck() {
+        return ((this.f1.weight >= minWeight && this.f1.weight <= maxWeight) && (this.f2.weight >= minWeight && this.f2.weight <= maxWeight));
+    }
+
+
+}
